c9d9ab682ba8553aabecffb25d4979e2c740c7ab
Roberto Yoc <A01172599@itesm.mx>
2017-10-25 23:09:09 -0500

    Materias y Profesores ya funcionan

---
+++ app/src/main/java/mx/com/talentics/myschool/layouts/alumnos.java
@@ -0,0 +1,13 @@
+package mx.com.talentics.myschool.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alumnos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alumnos);
+    }
+}
